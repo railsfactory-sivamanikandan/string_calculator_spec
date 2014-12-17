21bde2e7cd7b606ed0dc34c21a2e84adab9a91dc
sivamanikandan <sivamanikandan@railsfactory.org>
2014-12-17 10:51:18 +0530

    return zero for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,8 @@
 require_relative "../string_calculator"
 describe StringCalculator do
-
+  context "#manipulate" do
+   it "return 0 for empty string" do
+    expect("".extend(StringCalculator).manipulate).to eq(0)
+   end
+  end
 end
